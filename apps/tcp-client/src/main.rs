bf954f0b2ab56834b0b2df9fbffff15747ca583d
Gang Deng <13918383742@163.com>
2024-05-14 19:20:53 +0800

    send server key and run cal on server, send back result to client
    note: 字节序 ,from_ne_bytes 使用本地字节序

--- apps/tcp-client/src/main.rs
+++ apps/tcp-client/src/main.rs
@@ -1,16 +1,57 @@
 use std::net::{TcpStream};
 use std::io::{Read,Write};
 use std::str;
+use bincode;
+use tfhe::{ConfigBuilder, ServerKey, generate_keys, set_server_key, FheUint8,FheUint16};
+use tfhe::{ ClientKey,  FheInt16, FheUint,  FheUint16Id, FheUint32};
+use tfhe::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("tcp-client:Hello, world!");
+    println!("tcp-client:Creating key!");
+    let config = ConfigBuilder::default().build();
+    let ( client_key, server_key) = generate_keys(config);
+    let msg1 = 1u16;
+    let msg2 = 0u16;
+    println!("tcp-client:encrypting!");
+    let value_1 = FheUint16::encrypt(msg1, &client_key);
+    println!("tcp-client:encrypting!");
+    let value_2 = FheUint16::encrypt(msg2, &client_key);
+
+    let mut serialized_data = Vec::new();
+    println!("tcp-client:serializing server_key! {}",serialized_data.len());
+    bincode::serialize_into(&mut serialized_data, &server_key)?;
+    println!("tcp-client:serializing value_1! {}",serialized_data.len());
+    bincode::serialize_into(&mut serialized_data, &value_1)?;
+    println!("tcp-client:serializing value_2! {}",serialized_data.len());
+    bincode::serialize_into(&mut serialized_data, &value_2)?;
+
+
+    let steam_size :u128 = serialized_data.len() as u128;
     let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    let mut serialized_size = Vec::new();
+    bincode::serialize_into(&mut serialized_size, &steam_size)?;
+    println!("{},{},{},{}",serialized_size[0],serialized_size[1],serialized_size[2],serialized_size[3]);
 
-    stream.write("hello,rust.欢迎使用Rust".as_bytes()).unwrap();
+    stream.write(&serialized_size)?;
+    println!("tcp-client:sending {}!",serialized_data.len());
+    stream.write_all(&serialized_data)?;
+    stream.flush()?;
+    // stream.write("hello,rust.欢迎使用Rust".as_bytes()).unwrap();
 
-    let mut buffer = [0;1024];
+    // let mut buffer = [0;1024];
 
-    stream.read(&mut buffer).unwrap();
+    // stream.read(&mut buffer).unwrap();
 
-    println!("Response from server:{:?}",str::from_utf8(&buffer).unwrap().trim_matches('\u{0}'));
+    // println!("Response from server:{:?}",str::from_utf8(&buffer).unwrap().trim_matches('\u{0}'));
+    let mut serialized_result = Vec::new();
+    println!("tcp-client:receiving!");
+    stream.read_to_end(&mut serialized_result)?;
+    println!("tcp-client:deserializing!");
+    let result: FheUint8 = bincode::deserialize(&serialized_result)?;
+    println!("tcp-client:decrypting!");
+    let output: u16 = result.decrypt(&client_key);
+    assert_eq!(output, msg1 + msg2);
+    println!("OK!");
+    Ok(())
 }
--- apps/tcp-server/src/main.rs
+++ apps/tcp-server/src/main.rs
@@ -1,18 +1,68 @@
-use std::{net::TcpListener, io::{Read, Write}};
-
-fn main() {
+use std::{net::TcpListener, io::{self,Read, Write,Cursor}};
+use tfhe::{ConfigBuilder, ServerKey, generate_keys, set_server_key, FheUint8};
+use tfhe::prelude::*;
+fn main() -> io::Result<()> {
     println!("tcp-server: Hello, world!");
 
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     //当有client连接上来时
     for stream in listener.incoming(){
-        let mut stream = stream.unwrap();
         println!("Connection established!");
+        let mut stream = stream.unwrap();
+        let mut buffer = Vec::new();
+        println!("Recieving!");
+        let mut reading: bool = true;
+        let mut size_buffer = [0u8;16];
+        stream.read_exact(&mut size_buffer)?;
+        let total_size = u128::from_ne_bytes(size_buffer.clone());
+        println!("expect size is  {}",total_size);
+        println!("{},{},{},{}",size_buffer[0],size_buffer[1],size_buffer[2],size_buffer[3]);
+
+        while reading
+        {
+            let mut tmp_buffer:Vec<u8> = vec![0; 64*1024];
+            let readsize = stream.read(&mut tmp_buffer)?;
+            println!("readsize {}",readsize);
+            tmp_buffer.truncate(readsize);
+            buffer.append(&mut tmp_buffer);
+            println!("total size {} / {}",buffer.len() as u128,total_size);
+            if buffer.len() as u128 >=  total_size{
+                reading = false;
+            }
+        }
+        //let mut buffer = Vec::new();
         //创建1k的缓存区
-        let mut buffer = [0;1024];
+        //let mut buffer = [0;1024];
         //读取client发过来的内容
-        stream.read(&mut buffer).unwrap();
+        //stream.read(&mut buffer).unwrap();
         //原样送回去(相当于netty的EchoServer)
-        stream.write(&mut buffer).unwrap();
+        //stream.write(&mut buffer).unwrap();
+        let result = server_function(&buffer).unwrap();
+        println!("Sending!");
+        stream.write_all(&result)?;
+        stream.flush()?;
+        println!("Send!");
     }
+    Ok(())
+}
+
+fn server_function(serialized_data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut serialized_data = Cursor::new(serialized_data);
+    println!("Deserializing server key!");
+    let server_key: ServerKey = bincode::deserialize_from(&mut serialized_data)?;
+    println!("Deserializing server val 1!");
+    let ct_1: FheUint8 = bincode::deserialize_from(&mut serialized_data)?;
+    println!("Deserializing server val 2!");
+    let ct_2: FheUint8 = bincode::deserialize_from(&mut serialized_data)?;
+
+    println!("Set_server_key!");
+    set_server_key(server_key);
+
+    println!("Adding!");
+    let result = ct_1 + ct_2;
+
+    println!("Serialize!");
+    let serialized_result = bincode::serialize(&result)?;
+
+    Ok(serialized_result)
 }
